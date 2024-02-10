f7d28a65a2faeeb2afe184bf0b7d968c4a5cf7b8
Serena Rosignoli <68196372+SerenaRosi@users.noreply.github.com>
2024-02-10 11:45:15 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,92 +142,92 @@
     });
 })(jQuery);
 
-// CAROUSEL
-const carousel = document.querySelector(".carousel"),
-  firstImg = carousel.querySelectorAll("img")[0],
-  arrowIcons = document.querySelectorAll(".wrapper i");
+// // CAROUSEL
+// const carousel = document.querySelector(".carousel"),
+//   firstImg = carousel.querySelectorAll("img")[0],
+//   arrowIcons = document.querySelectorAll(".wrapper i");
 
-let isDragStart = false,
-  isDragging = false,
-  prevPageX,
-  prevScrollLeft,
-  positionDiff;
+// let isDragStart = false,
+//   isDragging = false,
+//   prevPageX,
+//   prevScrollLeft,
+//   positionDiff;
 
-const showHideIcons = () => {
-  // showing and hiding prev/next icon according to carousel scroll left value
-  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-  arrowIcons[1].style.display =
-    carousel.scrollLeft == scrollWidth ? "none" : "block";
-};
+// const showHideIcons = () => {
+//   // showing and hiding prev/next icon according to carousel scroll left value
+//   let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+//   arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+//   arrowIcons[1].style.display =
+//     carousel.scrollLeft == scrollWidth ? "none" : "block";
+// };
 
-arrowIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-    setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-  });
-});
+// arrowIcons.forEach((icon) => {
+//   icon.addEventListener("click", () => {
+//     let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+//     // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+//     carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+//     setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+//   });
+// });
 
-const autoSlide = () => {
-  // if there is no image left to scroll then return from here
-  if (
-    carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
-    carousel.scrollLeft <= 0
-  )
-    return;
+// const autoSlide = () => {
+//   // if there is no image left to scroll then return from here
+//   if (
+//     carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
+//     carousel.scrollLeft <= 0
+//   )
+//     return;
 
-  positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-  let firstImgWidth = firstImg.clientWidth + 14;
-  // getting difference value that needs to add or reduce from carousel left to take middle img center
-  let valDifference = firstImgWidth - positionDiff;
+//   positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+//   let firstImgWidth = firstImg.clientWidth + 14;
+//   // getting difference value that needs to add or reduce from carousel left to take middle img center
+//   let valDifference = firstImgWidth - positionDiff;
 
-  if (carousel.scrollLeft > prevScrollLeft) {
-    // if user is scrolling to the right
-    return (carousel.scrollLeft +=
-      positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
-  }
-  // if user is scrolling to the left
-  carousel.scrollLeft -=
-    positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-};
+//   if (carousel.scrollLeft > prevScrollLeft) {
+//     // if user is scrolling to the right
+//     return (carousel.scrollLeft +=
+//       positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
+//   }
+//   // if user is scrolling to the left
+//   carousel.scrollLeft -=
+//     positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+// };
 
-const dragStart = (e) => {
-  // updatating global variables value on mouse down event
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevScrollLeft = carousel.scrollLeft;
-};
+// const dragStart = (e) => {
+//   // updatating global variables value on mouse down event
+//   isDragStart = true;
+//   prevPageX = e.pageX || e.touches[0].pageX;
+//   prevScrollLeft = carousel.scrollLeft;
+// };
 
-const dragging = (e) => {
-  // scrolling images/carousel to left according to mouse pointer
-  if (!isDragStart) return;
-  e.preventDefault();
-  isDragging = true;
-  carousel.classList.add("dragging");
-  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-  showHideIcons();
-};
+// const dragging = (e) => {
+//   // scrolling images/carousel to left according to mouse pointer
+//   if (!isDragStart) return;
+//   e.preventDefault();
+//   isDragging = true;
+//   carousel.classList.add("dragging");
+//   positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+//   carousel.scrollLeft = prevScrollLeft - positionDiff;
+//   showHideIcons();
+// };
 
-const dragStop = () => {
-  isDragStart = false;
-  carousel.classList.remove("dragging");
+// const dragStop = () => {
+//   isDragStart = false;
+//   carousel.classList.remove("dragging");
 
-  if (!isDragging) return;
-  isDragging = false;
-  autoSlide();
-};
+//   if (!isDragging) return;
+//   isDragging = false;
+//   autoSlide();
+// };
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("touchstart", dragStart);
 
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
+// document.addEventListener("mousemove", dragging);
+// carousel.addEventListener("touchmove", dragging);
 
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
+// document.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("touchend", dragStop);
 
 // New code to handle image zoom
 // const zoomImage = (e) => {
@@ -273,57 +273,210 @@ carousel.addEventListener("touchend", dragStop);
 //   carousel.addEventListener("click", zoomCarousel);
 // });
 
-let currentImageIndex = 0; // Track the currently displayed image index
+// let currentImageIndex = 0; // Track the currently displayed image index
 
-const zoomCarousel = (e) => {
-  const carousel = e.target.closest(".wrapper"); // Find the closest carousel container
+// const zoomCarousel = (e) => {
+//   const carousel = e.target.closest(".wrapper"); // Find the closest carousel container
 
-  if (carousel) {
-    const images = carousel.querySelectorAll(".carousel img");
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
+//   if (carousel) {
+//     const images = carousel.querySelectorAll(".carousel img");
+//     const overlay = document.createElement("div");
+//     overlay.classList.add("overlay");
 
-    const closeButton = document.createElement("span");
-    closeButton.innerHTML = "&#x2716;"; // Unicode "X" symbol
-    closeButton.classList.add("close-button");
-    closeButton.addEventListener("click", () => {
-      overlay.remove();
-    });
+//     const closeButton = document.createElement("span");
+//     closeButton.innerHTML = "&#x2716;"; // Unicode "X" symbol
+//     closeButton.classList.add("close-button");
+//     closeButton.addEventListener("click", () => {
+//       overlay.remove();
+//     });
 
-    const carouselContainer = document.createElement("div");
-    carouselContainer.classList.add("zoomed-carousel-container");
+//     const carouselContainer = document.createElement("div");
+//     carouselContainer.classList.add("zoomed-carousel-container");
 
-    const imageElement = document.createElement("img");
-    imageElement.classList.add("zoomed-img");
-    imageElement.src = images[currentImageIndex].src;
+//     const imageElement = document.createElement("img");
+//     imageElement.classList.add("zoomed-img");
+//     imageElement.src = images[currentImageIndex].src;
 
-    const prevButton = document.createElement("button");
-    prevButton.innerHTML = "&#10094;"; // Unicode left arrow
-    prevButton.classList.add("carousel-nav");
-    prevButton.addEventListener("click", () => {
-      currentImageIndex =
-        (currentImageIndex - 1 + images.length) % images.length;
-      imageElement.src = images[currentImageIndex].src;
-    });
+//     const prevButton = document.createElement("button");
+//     prevButton.innerHTML = "&#10094;"; // Unicode left arrow
+//     prevButton.classList.add("carousel-nav");
+//     prevButton.addEventListener("click", () => {
+//       currentImageIndex =
+//         (currentImageIndex - 1 + images.length) % images.length;
+//       imageElement.src = images[currentImageIndex].src;
+//     });
 
-    const nextButton = document.createElement("button");
-    nextButton.innerHTML = "&#10095;"; // Unicode right arrow
-    nextButton.classList.add("carousel-nav");
-    nextButton.addEventListener("click", () => {
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-      imageElement.src = images[currentImageIndex].src;
-    });
+//     const nextButton = document.createElement("button");
+//     nextButton.innerHTML = "&#10095;"; // Unicode right arrow
+//     nextButton.classList.add("carousel-nav");
+//     nextButton.addEventListener("click", () => {
+//       currentImageIndex = (currentImageIndex + 1) % images.length;
+//       imageElement.src = images[currentImageIndex].src;
+//     });
 
-    carouselContainer.appendChild(prevButton);
-    carouselContainer.appendChild(imageElement);
-    carouselContainer.appendChild(nextButton);
-    overlay.appendChild(closeButton);
-    overlay.appendChild(carouselContainer);
-    document.body.appendChild(overlay);
-  }
-};
+//     carouselContainer.appendChild(prevButton);
+//     carouselContainer.appendChild(imageElement);
+//     carouselContainer.appendChild(nextButton);
+//     overlay.appendChild(closeButton);
+//     overlay.appendChild(carouselContainer);
+//     document.body.appendChild(overlay);
+//   }
+// };
 
-const carousels = document.querySelectorAll(".wrapper img");
-carousels.forEach((carousel) => {
-  carousel.addEventListener("click", zoomCarousel);
+// const carousels = document.querySelectorAll(".wrapper img");
+// carousels.forEach((carousel) => {
+//   carousel.addEventListener("click", zoomCarousel);
+// });
+
+// SWIPER 
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 10,
+  freeMode: true,
+  watchSlidesProgress: true,
 });
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// GOOGLE TRANSLATE 
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+
+// GALLERY LIGTHBOX
+
+// Get modal element
+var modal = document.getElementById('lightbox-modal');
+var modalImg = document.getElementById("lightbox-img");
+var lightboxCaption = document.getElementById('lightbox-caption'); // Get the caption element
+var images = document.querySelectorAll('.gallery-item img');
+var currentIndex = 0;
+
+// Open modal
+images.forEach((img, index) => {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        currentIndex = index;
+        // Set caption
+        var captionText = img.nextElementSibling ? img.nextElementSibling.innerHTML : ''; // Check if there's a figcaption
+        lightboxCaption.innerHTML = captionText;
+    }
+});
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+// Change image (previous or next)
+function changeImg(step) {
+    currentIndex += step;
+    if (currentIndex >= images.length) currentIndex = 0;
+    if (currentIndex < 0) currentIndex = images.length - 1;
+    modalImg.src = images[currentIndex].src;
+    // Update caption
+    var newCaptionText = images[currentIndex].nextElementSibling ? images[currentIndex].nextElementSibling.innerHTML : ''; // Check if there's a figcaption
+    lightboxCaption.innerHTML = newCaptionText;
+}
+
+// Close modal if outside click
+window.onclick = function(event) {
+    console.log(event.target)
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// MAIN GALLERY LIGTHBOX
+
+// Get modal element
+var main_modal = document.getElementById('main-lightbox-modal');
+var main_modalImg = document.getElementById("main-lightbox-img");
+// var main_lightboxCaption = document.getElementById('main-lightbox-caption'); // Get the caption element
+var main_images = document.querySelectorAll('.main-gallery-item img');
+var main_currentIndex = 0;
+
+// Open modal
+main_images.forEach((mimg, mindex) => {
+    mimg.onclick = function() {
+        main_modal.style.display = "block";
+        main_modalImg.src = this.src;
+        main_currentIndex = mindex;
+        // Set caption
+        // var captionText = mimg.nextElementSibling ? mimg.nextElementSibling.innerHTML : ''; // Check if there's a figcaption
+        // main_lightboxCaption.innerHTML = captionText;
+    }
+});
+
+// Close modal
+var main_span = document.getElementsByClassName("main-close")[0];
+main_span.onclick = function() { 
+    main_modal.style.display = "none";
+}
+
+// Change image (previous or next)
+function main_changeImg(step) {
+    currentIndex += step;
+    if (currentIndex >= main_images.length) main_currentIndex = 0;
+    if (currentIndex < 0) main_currentIndex = main_images.length - 1;
+    main_modalImg.src = main_images[currentIndex].src;
+    // Update caption
+    var newCaptionText = main_images[currentIndex].nextElementSibling ? main_images[main_currentIndex].nextElementSibling.innerHTML : ''; // Check if there's a figcaption
+    // main_lightboxCaption.innerHTML = newCaptionText;
+}
+
+// Close modal if outside click
+window.onclick = function(event) {
+    if (event.target == main_modal) {
+        main_modal.style.display = "none";
+    }
+}
+
+// Variables to store touch points
+var touchStartX = 0;
+var touchEndX = 0;
+
+// Function to handle the start of a touch
+function touchStart(event) {
+    touchStartX = event.touches[0].clientX;
+}
+
+// Function to handle the move of a touch
+function touchMove(event) {
+    touchEndX = event.touches[0].clientX;
+}
+
+// Function to handle the end of a touch
+function touchEnd() {
+    if (touchEndX < touchStartX) {
+        changeImg(1); // Swipe Left, Next Image
+    }
+    if (touchEndX > touchStartX) {
+        changeImg(-1); // Swipe Right, Previous Image
+    }
+}
+
+// Add touch event listeners to the modal image
+modalImg.addEventListener('touchstart', touchStart, false);
+modalImg.addEventListener('touchmove', touchMove, false);
+modalImg.addEventListener('touchend', touchEnd, false);
+
+// Add touch event listeners to the modal image
+main_modalImg.addEventListener('touchstart', touchStart, false);
+main_modalImg.addEventListener('touchmove', touchMove, false);
+main_modalImg.addEventListener('touchend', touchEnd, false);
